b89bcdd060dec05215c260cfeb541f2f0e603369
Gordon Seto <gordon.seto@gmail.com>
2017-03-12 19:09:51 -0700

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Gordon Seto on 2017-03-12.
+//  Copyright © 2017 Gordon Seto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
